a87b62545cbfbd1753b0368832ce5f8480f126e4
WebDeveloperKomal <76107750+WebDeveloperKomal@users.noreply.github.com>
2023-09-08 18:02:51 +0530

    Add files via upload

---
+++ Contact-Us.js
@@ -0,0 +1,28 @@
+function saveContactUsForm(){
+
+    var contactUsForm = {
+        name : document.getElementById("name").value ,
+        email : document.getElementById("email").value ,
+        phoneNumber : document.getElementById("phoneNumber").value ,
+        services : document.getElementById("services").value ,
+        message : document.getElementById("message").value ,
+    }
+
+    console.log(JSON.stringify(contactUsForm));
+
+
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    headers.append('Accept', 'application/json');
+
+
+    fetch("http://localhost:8080/contact-us" ,{
+        method : 'POST',
+        body : JSON.stringify(contactUsForm),
+        headers : headers,
+    })
+      
+
+    .then(response => response.json())
+    .then(json => console.log(json))
+}
---
+++ index.js
@@ -0,0 +1,28 @@
+function saveContactForm(){
+
+    var contactform = {
+        fullName : document.getElementById('fullName').value,
+        email : document.getElementById('email').value,
+        phoneNumber : document.getElementById('phoneNumber').value,
+        company : document.getElementById('company').value,
+        message : document.getElementById('message').value
+    }
+
+    console.log(JSON.stringify(contactform));
+
+    let headers=  new Headers();
+    headers.append('Content-Type', 'application/json');
+    headers.append('Accept', 'application/json');
+
+
+    fetch("http://localhost:8080/save-form",{
+        method : 'POST',
+        body : JSON.stringify(contactform),
+        headers : headers,
+    })
+
+    .then(response => response.json())
+    .then(json => console.log(json))
+    
+
+}
